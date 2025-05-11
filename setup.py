e841c88136c1cd2134e88138ef8008af83345301
Abdullah <1xcoder@proton.me>
2025-05-11 20:31:31 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
     name='pyqt-image-to-text',
     version='0.0.1',
     author='Jung Gyu Yoon',
-    author_email='yjg30737@gmail.com',
+    author_email='tsjoker14311211@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt GUI example of image to text using image-to-text model',
-    url='https://github.com/yjg30737/pyqt-image-to-text.git',
+    url='https://github.com/1xcoder-1/pyqt-image-to-text.git',
     long_description_content_type='text/markdown',
     long_description=long_description,
     install_requires=[
